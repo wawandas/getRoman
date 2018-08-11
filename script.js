f45d02b627e8f3c5386b3d27093e217c7da59e26
wawandas <vadim.psarev@segmentify.com>
2018-08-11 16:52:31 +0200

    add roman to arabic object

--- script.js
+++ script.js
@@ -6,5 +6,6 @@
 
 
 function func(number) {
-
+	//in this object each roman number represents arabic number
+	var romanToArabic = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
 }
